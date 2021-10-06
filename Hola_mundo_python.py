86790b8cd8e6d04a9549dc185c11bcb9e8d14cbf
JENNI-ALMARAZ <91512122+JENNI-ALMARAZ@users.noreply.github.com>
2021-10-05 21:23:22 -0500

    Hola mundo en Python
    Se presenta un Hola mundo en lenguaje Python, ejecutado y compilado desde líneas de comando en vim.

---
+++ Hola_mundo_python.py
@@ -0,0 +1,9 @@
+# Hola Mundo en Python 
+# Title         :Hola Mundo en Python 
+# Description   :Hola mundo en Python desde vim en windows 
+# Author        :Jennifer Almaraz Suarez 
+# Date          :20211005
+# Version       :1
+# Usage         :c:\>python /path/Hola_mundo_python.py
+
+print ("Primer hola mundo en Python desde vim en windows")
