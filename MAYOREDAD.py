16b8cbc7704542671b426ee47c271a8e4460a835
JENNI-ALMARAZ <91512122+JENNI-ALMARAZ@users.noreply.github.com>
2021-10-19 16:09:07 -0500

    ALGORITMO DE EDAD EN PYTHON
    Mediante nuestro año de nacimiento determinamos si somos o no mayores de edad

---
+++ MAYOREDAD.py
@@ -0,0 +1,13 @@
+#################################
+#Fecha: 19-10-2021              #
+#Autor: Jennifer Almaraz Suarez #
+#<almarazjennifer44@gmail.com   #
+#################################
+
+fecha_Nacimiento = int (input("Digite su año de nacimiento: "))
+diferencia (2021 - fecha_Nacimiento)
+
+if diferencia >= 18:
+    print ("El usuario tiene", diferencia ," es mayor de edad")
+else: 
+    print ("El usuario tiene", diferencia ," es menor de edad")    
